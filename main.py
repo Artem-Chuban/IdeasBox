4c046750df7391ab7aa39ea50f78849f0ca49e1d
ArtemChuban <ArtemChuban@yandex.ru>
2023-04-21 16:08:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Idea(BaseModel):
+    name: str
+    description: str
+
+
+class Ideas:
+    def __init__(self):
+        self.__ideas: list[Idea] = []
+
+    def append(self, idea: Idea) -> None:
+        self.__ideas.append(idea)
+
+    def get(self, offset: int, count: int) -> list[Idea]:
+        return self.__ideas[offset:offset + count]
+
+
+app = FastAPI()
+ideas = Ideas()
+
+
+@app.get("/idea")
+async def get_idea(offset: int = 0, count: int = 10) -> list[Idea]:
+    return ideas[offset:offset + count]
+
+
+@app.post("/idea")
+async def post_idea(idea: Idea):
+    ideas.append(idea)
