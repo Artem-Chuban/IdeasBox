d2c7174b8bfc3667fbb592cefecd1f8c57d5b3d2
ArtemChuban <ArtemChuban@yandex.ru>
2023-04-21 16:45:01 +0200

    Docker init

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ ideas = Ideas()
 
 @app.get("/idea")
 async def get_idea(offset: int = 0, count: int = 10) -> list[Idea]:
-    return ideas[offset:offset + count]
+    return ideas.get(offset, count)
 
 
 @app.post("/idea")
